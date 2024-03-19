c66308e0ce64a16e9517b28fd6d3f4fde829491f
aridoniawan <aridoniawan1@gmail.com>
2024-03-19 10:54:41 +0700

    Menginstal VueCLI Pertama Kali

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mt-5">
+    <h1 class="animate__fadeInLeft">IDShops</h1>
+    <P class="animate__animated animate__fadeInLeft">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio a quod, excepturi, recusandae inventore facilis, eius ut libero ipsa quibusdam porro? Voluptatum dicta minima, rem sint iusto sit. Nesciunt dolores sint dignissimos hic ab dolor laborum repellat, 
+      adipisci inventore esse id repudiandae odio impedit itaque, necessitatibus corporis ad iure. Id.
+    </P>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FontAwesomeIcon,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
